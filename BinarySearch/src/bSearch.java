4cff5bd00a3fa077dc1076189f7f2f16429f8805
fellfromatree <paul@caseyfamily.co.uk>
2021-01-31 17:23:50 +0700

    Initial Upload with comments

---
+++ BinarySearch/src/bSearch.java
@@ -0,0 +1,47 @@
+
+public class bSearch 
+{
+	
+	public bSearch(int target)
+	{
+		// Initialise data structures
+		int[] intArray = {1, 14, 16, 23, 42, 53, 67, 93, 212, 537};
+		
+		// Set high and low pointers and found position
+		
+		// While the item hasn't been found and low is <= high
+		
+		// Calculate the midpoint
+		
+		// If midpoint is the search item
+		
+		// Update found position
+		
+		// If search item > midpoint
+		
+		// Update low
+		
+		// Otherwise
+		
+		// Update high
+		
+		
+		// If foundAt is >=0
+		
+		// Print found position
+		
+		// Otherwise
+		
+		// Print error message
+	}
+	
+	public static void main(String[] args)
+	{
+		// Input target number
+		
+		// Create new instance of bSearch
+		
+		
+	}
+
+}
