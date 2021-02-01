9aba9533339d4c101b43ca44bc88c8aefd910691
fellfromatree <paul@caseyfamily.co.uk>
2021-02-01 09:34:46 +0700

    changes...

--- BinarySearch/src/bSearch.java
+++ BinarySearch/src/bSearch.java
@@ -8,7 +8,7 @@ public class bSearch
 		int[] intArraySorted = {1, 14, 16, 23, 42, 53, 67, 93, 212, 537};
 		int[] intArrayUnsorted = {1, 212, 16, 67, 42, 53, 23, 14, 93, 537};
 		// Set high and low pointers and found position
-		
+		// here's a change
 		// While the item hasn't been found and low is <= high
 		
 		// Calculate the midpoint
