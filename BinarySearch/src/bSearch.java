386749b15672b395c99fc054d4c60dcbaeab7fff
fellfromatree <paul@caseyfamily.co.uk>
2021-02-01 15:01:22 +0700

    Added code to the comments

--- BinarySearch/src/bSearch.java
+++ BinarySearch/src/bSearch.java
@@ -1,45 +1,67 @@
+import java.util.Scanner;
 
 public class bSearch 
 {
 	
 	public bSearch(int target)
 	{
-		// Initialise data structures
-		int[] intArraySorted = {1, 14, 16, 23, 42, 53, 67, 93, 212, 537};
-		int[] intArrayUnsorted = {1, 212, 16, 67, 42, 53, 23, 14, 93, 537};
+		// Initialize data structures & Variables
+		int[] arrayNums = {1, 212, 16, 67, 42, 53, 23, 14, 93, 537};
+		
 		// Set high and low pointers and found position
-		// here's a change
+		int low = 0;
+		int high = arrayNums.length-1;
+		int foundAt = -1; // pointer to the location of the found item
+		
 		// While the item hasn't been found and low is <= high
-		
+		while(foundAt==-1 && low<=high)
+		{
 		// Calculate the midpoint
+			int mid = (low+high)/2;
 		
-		// If midpoint is the search item
-		
-		// Update found position
-		
-		// If search item > midpoint
-		
-		// Update low
-		
-		// Otherwise
-		
-		// Update high
+			// If midpoint is the search item
+			if(arrayNums[mid]==target)
+			{
+				// Update found position
+				foundAt = mid;
+			}
+			// If search item > midpoint
+			else if(target>arrayNums[mid])
+			{
+				// Update low
+				low = mid+1;
+			}
+			// Otherwise
+			else
+			{
+				// Update high	
+				high = mid-1;
+			}
+		}
 		
 		
 		// If foundAt is >=0
-		
+		if( foundAt>=0)
+		{
 		// Print found position
-		
+			System.out.println("Found item at position "+foundAt);
 		// Otherwise
-		
+		}
+		else
+		{
 		// Print error message
+			System.out.println("Item doesn't exist in the list");
+		}
 	}
 	
 	public static void main(String[] args)
 	{
 		// Input target number
-		
+		int t = 0;
+		Scanner kb = new Scanner(System.in);
+		t = kb.nextInt();
 		// Create new instance of bSearch
+		bSearch b1 = new bSearch(t);
 		
 		
 	}
