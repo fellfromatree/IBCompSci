5b3a65b143061880751cc767ae51173611ca7e25
fellfromatree <paul@caseyfamily.co.uk>
2021-02-22 14:46:01 +0700

    Selection sort upload

--- Sorting/src/bubble/bSort.java
+++ Sorting/src/bubble/bSort.java
@@ -11,9 +11,12 @@ public class bSort {
 	public bSort()
 	{
 		int[] arrayNums = {66, 13, 7, 67, 24, 113, 6, 89, 27, 38};
+		//int[] arrayNums = {6,7,13,24,27,38,66,67,89,113};
 		printArray(arrayNums);
 		arrayNums = sortThis(arrayNums);
+		
 		printArray(arrayNums);
+		
 	}
 	
 	
@@ -31,20 +34,32 @@ public class bSort {
 	{
         int n = array.length;
         int temp = 0;
+        int compare=0;
+        int swaps=0;
+        int x=0;
+        boolean ordered = false;
         
-        for (int x = 0; x<n; x++)
+        while(x<n && !ordered)
         {
+        	ordered=true;
         	for(int y = 0; y<(n-x)-1;y++)
         	{
+        		compare++;
         		if(array[y]>array[y+1])
         		{
         			// SWAP
         			temp = array[y];
         			array[y]=array[y+1];
         			array[y+1]=temp;
+        			swaps++;
+        			ordered=false;
         		}
         	}
+        	x++;
         }
+        
+        System.out.println("Comparisons made "+compare);
+        System.out.println("Swaps made "+swaps);
 		
 		return array;
 	}
---
+++ Sorting/src/selection/sSort.java
@@ -0,0 +1,63 @@
+package selection;
+
+public class sSort {
+
+	public static void main(String[] args)
+	{
+		sSort s1 = new sSort();
+	
+	}
+	
+	public sSort()
+	{
+		int[] arrayNums = {66, 13, 7, 67, 24, 113, 6, 89, 27, 38};
+		//int[] arrayNums = {6,7,13,24,27,38,66,67,89,113};
+		printArray(arrayNums);
+		arrayNums = sortThis(arrayNums);
+		
+		printArray(arrayNums);
+	}
+	
+	
+	public void printArray(int[] array)
+	{
+		System.out.println("Current array state:");
+		for(int x = 0; x< array.length; x++)
+		{
+			System.out.print(array[x] + ", ");
+		}
+		System.out.print("\n");
+	}
+	
+	
+	
+	public int[] sortThis(int[] array)
+	{
+        int n = array.length;
+        int temp = 0;
+        int minPos = 0;
+        
+        // Loop from 0 to n-2
+        
+        // Set minPos = first item
+        
+        // Loop from x + 1 to n-1
+        
+        // If current item < min item
+        
+        // minPos = current position
+        
+        // End if
+        
+        // End loop
+        
+        // Swap
+        
+        // End loop
+        
+
+		
+		return array;
+	}
+	
+}
