c71517759fbd9920ae2bdf0fc915430f542ca2b9
fellfromatree <paul@caseyfamily.co.uk>
2021-04-01 14:37:19 +0700

    Updated to include newUser method in instagram.java. This will be used when creating a collection of users.

--- instaFamous/src/iF/instagram.java
+++ instaFamous/src/iF/instagram.java
@@ -1,10 +1,26 @@
 package iF;
+import java.util.Scanner;
 
 public class instagram {
 
 	public static void main(String[] args) {
 		influencer i1 = new influencer("Paul","Vietnam","mrc");
 
+	}
+	
+	public void newUser()
+	{
+		System.out.println("Instagram Signup");
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter your full name");
+		String n = kb.nextLine();
+		System.out.println("Which country do you live in?");
+		String c = kb.nextLine();
+		System.out.println("Please choose your username");
+		String un = kb.nextLine();
+		influencer i1 = new influencer(n,c,un);
+		i1.retrieveDetails();
+		i1.evaluateProfile();
 	}
 
 }
