95f1b0e019e293a788abdb74c27151e1dc315d13
Paul Casey <paul@caseyfamily.co.uk>
2021-10-05 22:49:09 +0800

    Uploads

---
+++ javaIOPractice/src/javaIOPractice/ErrorCatching.java
@@ -0,0 +1,47 @@
+package javaIOPractice;
+
+import java.util.Scanner;
+
+public class ErrorCatching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ErrorCatching e1 = new ErrorCatching();
+	}
+	
+	
+	public ErrorCatching()
+	{
+		int n = getNum(1,100);
+		System.out.println(n);
+	}
+	
+	
+	public int getNum(int low, int high)
+	{
+		Scanner kb = new Scanner(System.in);
+		int n=0;
+		boolean success = false;
+		
+		while(!success)
+		{
+			System.out.println("Please enter a number "+low+" - "+high);
+			String ui = kb.nextLine();
+			try
+			{
+				n = Integer.parseInt(ui);
+				if (n>=low && n<=high)
+				{
+					success=true;
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("You did not enter a number");
+			}
+		}
+		
+		return n;
+	}
+
+}
